e78760528a049d27ac9a38ef42bde879c131d444
BuildTools <unconfigured@null.spigotmc.org>
2022-03-13 15:33:20 +0200

    подреждане и 3та задача от 10.2

---
+++ 03/10.2/03 - 10.2 - 3та задача.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int count = int.Parse(Console.ReadLine());
+            int[] sum = new int[nums.Length];
+            int k = nums[0];
+            int k2 = nums[0];
+            for (int i = 0; i < count; i++)
+            {
+                int last = nums[nums.Length - 1];
+                for (int j = 0; j < nums.Length-1; j++)
+                {
+                    k2 = nums[j+1];
+                    nums[j+1] = k;
+                    k = k2;
+                }
+                nums[0] = last;
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    sum[j] = sum[j] + nums[j];
+                }
+            }
+            Console.WriteLine(string.Join(" ", sum));
+        }
+    }
+}
