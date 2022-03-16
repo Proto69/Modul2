e73de9a39e8096bfa3d2c0562c92d7b635c8b844
BuildTools <unconfigured@null.spigotmc.org>
2022-03-16 07:22:42 +0200

    12 е готово и малко поправки в имената

---
+++ 03/12/4-та задача.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp150
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] arr2 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] result = new int[arr1.Length + arr2.Length];
+            arr1.CopyTo(result, 0);
+            arr2.CopyTo(result, arr1.Length);
+            Array.Sort(result);
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
---
+++ 03/12/5-та задача.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp150
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //според мен така се решава задачата
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Array.Sort(arr);
+            //ако гледаме примера ще видим, че най-големият елемент липсва
+            //за да го махнем просто пишем
+            int[] result = new int[arr.Length - 1];
+            Array.Copy(arr, result, arr.Length - 1);
+            //и след това принтим вместо arr, result
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
