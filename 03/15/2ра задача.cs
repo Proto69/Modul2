3319a03c917dfaab0c95843d0944b7062f646ce9
BuildTools <unconfigured@null.spigotmc.org>
2022-03-19 08:58:10 +0200

    нз

---
+++ 03/15/2ра задача.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp150
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            int remove = nums[nums.Count - 1];
+            nums = nums.Where(x => x != remove).ToList();
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
