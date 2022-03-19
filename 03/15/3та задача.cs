6b2d5ff4a3dbd1fb8690fbadb03ae83e7e6089c0
BuildTools <unconfigured@null.spigotmc.org>
2022-03-19 14:49:59 +0200

    работа по 15

---
+++ 03/15/3та задача.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp150
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nums = Console.ReadLine().Split("|").Where(x => x != " ").ToList();
+            for (int i = 0; i < nums.Count; i++)
+            {
+                nums[i] = Reverse(nums[i]);
+            }
+            string a = string.Join(" ", nums);
+            nums = a.Split(" ").Where(x => x != "" && x != null && x != " ").Reverse().ToList();
+            Console.WriteLine(string.Join(" ", nums));
+        }
+        static string Reverse(string num1)
+        {
+            char[] charArray = num1.ToCharArray();
+            Array.Reverse(charArray);
+            return string.Join("", charArray);
+        }
+    }
+}
