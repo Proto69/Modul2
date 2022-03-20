d414404ce277e53ca250a765dba7bd6c445c1d31
BuildTools <unconfigured@null.spigotmc.org>
2022-03-20 10:04:56 +0200

    updates

--- 03/15/3та задача.cs
+++ 03/15/3та задача.cs
@@ -8,20 +8,10 @@ namespace ConsoleApp150
     {
         static void Main(string[] args)
         {
-            List<string> nums = Console.ReadLine().Split("|").Where(x => x != " ").ToList();
-            for (int i = 0; i < nums.Count; i++)
-            {
-                nums[i] = Reverse(nums[i]);
-            }
-            string a = string.Join(" ", nums);
-            nums = a.Split(" ").Where(x => x != "" && x != null && x != " ").Reverse().ToList();
-            Console.WriteLine(string.Join(" ", nums));
-        }
-        static string Reverse(string num1)
-        {
-            char[] charArray = num1.ToCharArray();
-            Array.Reverse(charArray);
-            return string.Join("", charArray);
+            List<int> nums = Console.ReadLine().Split(" ").Select(int.Parse).Where(x => x >= 0).ToList();
+            nums.Reverse();
+            if (nums.Count == 0) Console.WriteLine("Empty");
+            else Console.WriteLine(string.Join(" ", nums));
         }
     }
 }
---
+++ 03/15/5та задача.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp150
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            List<int> bombAndPower = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] == bombAndPower[0])
+                {
+                    for (int j = 0; j <= bombAndPower[1]; j++)
+                    {
+                        try
+                        {
+                            nums[i + j] = 0;
+                        }
+                        catch { }
+                        try
+                        {
+                            nums[i - j] = 0;
+                        }
+                        catch { }
+                    }
+                }
+            }
+            int sum = 0;
+            foreach (int num in nums)
+            {
+                sum += num;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
