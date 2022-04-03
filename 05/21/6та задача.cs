cb0aa0b47a4d35fe993e375e266dca009c168a65
Proto69 <timkaq67@gmail.com>
2022-04-03 20:00:09 +0300

    21 e готово

---
+++ 05/21/6та задача.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestForPalindrom(Console.ReadLine());
+        }
+
+        static void TestForPalindrom(string word)
+        {
+            string a = "";
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                a += word[i];
+            }
+            if (a == word) Console.WriteLine("True");
+            else Console.WriteLine("False");
+        }
+    }
+}
